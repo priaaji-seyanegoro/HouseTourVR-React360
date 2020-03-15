2a4851a15034daf8798116ddc58656c151e0cce2
priaajisn <priaaji.setyanegoro@gmail.com>
2020-03-16 03:18:13 +0700

    change static_asset & modify dataHouse

--- client.js
+++ client.js
@@ -19,7 +19,7 @@ function init(bundle, parent, options = {}) {
   );
 
   // Load the initial environment
-  r360.compositor.setBackground(r360.getAssetURL("360_front.jpg"));
+  r360.compositor.setBackground(r360.getAssetURL("360_front_door.jpg"));
 }
 
 window.React360 = { init };
